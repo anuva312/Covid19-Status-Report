642b2121af8ef8a7bd6a0a3d66be434351a3d4cb
ANUSREE V A <anjioangel@gmail.com>
2020-07-07 12:50:00 +0530

    A bit more modifications to autocomplete.

--- js/json_country.js
+++ js/json_country.js
@@ -4,12 +4,13 @@
   function (event) {
     var summary;
     var country_list=[];
+    // var got_list = false;
     //To get data pertaining to a specific country 
     function getCountryData(country_name){
       var country_data = summary["Countries"]
               // console.log(country_name)
       country_data.forEach(function (each_country){
-        country_list.push(each_country.Country);
+        // country_list.push(each_country.Country);
         if (each_country.Country === country_name){
           console.log(each_country);
           document.querySelector("#country_field_head")
@@ -20,14 +21,24 @@
             .innerHTML = ("Deaths: " +each_country["TotalDeaths"]);
           document.querySelector("#country_recovered")
             .innerHTML = ("Recovered: " +each_country["TotalRecovered"]);
+          // if(got_list===true){
+          //   break;
+          // }
         } 
       })
+    };
+
+    function getCountryList(){
+      summary['Countries'].forEach(function (each_country){
+        country_list.push(each_country.Country);
+      });
     };
 
     //Accessing JSON file
     $ajaxUtils.sendGetRequest("https://api.covid19api.com/summary", 
     function (request) {
       summary = JSON.parse(request.responseText);
+      getCountryList();
       var global_count = summary["Global"]
       document.querySelector("#global_confirmed")
         .innerHTML = ("Confirmed: " +global_count["TotalConfirmed"]);
@@ -36,7 +47,6 @@
       document.querySelector("#global_recovered")
         .innerHTML = ("Recovered: " +global_count["TotalRecovered"]);
       getCountryData("United States of America");
-
       document.querySelector("button").addEventListener("click",
       function () {
         var country_name = document.getElementById("country_field").value;
